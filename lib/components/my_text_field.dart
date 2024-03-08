c96a9e800e8aeb51b075ae55db118406cd09a0f7
shubham thorat <shubham230520011@gmail.com>
2024-03-08 14:15:04 +0530

    first half commit

--- lib/components/my_text_field.dart
+++ lib/components/my_text_field.dart
@@ -4,6 +4,7 @@ class MyTextField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
   final bool obscureText;
+  
   final TextInputType keyboardType;
   final Widget? suffixIcon;
   final VoidCallback? onTap;
